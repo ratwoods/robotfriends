7b3282afb8e43ed34c26b810fb7a4c5f301f56fd
ratwoods <56761295+ratwoods@users.noreply.github.com>
2020-12-13 06:22:28 +0100

    full app , for now!!

---
+++ src/containers/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import Cardlist from '../components/Cardlist';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import './App.css';
+
+
+class App extends Component{
+	constructor() {
+		super()
+		this.state = {
+			robots: [],
+			searchfield: ''
+		}
+	}
+	componentDidMount(){
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response=> response.json())
+		.then(users => {this.setState({ robots: users})});
+		
+	}
+
+	onSearchchange=(event) => {
+		this.setState({ searchfield: event.target.value })	
+	}
+	render() {
+		const {robots, searchfield } = this.state;
+		const filteredRobots = robots.filter(robot =>{
+			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+		})
+		return!robots.length ?
+		  <h1>loading</h1>:
+		 
+			(
+				<div className= 'tc'>
+					<h1 className= 'f1'>RobotFriends</h1>
+					<SearchBox searchChange={this.onSearchchange}/>
+					<Scroll>
+						<Cardlist robots={filteredRobots} />
+					</Scroll>
+				</div>
+			);
+	}
+	
+}
+
+export default App;
